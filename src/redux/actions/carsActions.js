ff3928ff2b5d09b554e1b63a3b14f3b442f31735
gokul299 <83939754+gokul299@users.noreply.github.com>
2022-03-18 23:16:23 +0530

    Update carsActions.js

--- src/redux/actions/carsActions.js
+++ src/redux/actions/carsActions.js
@@ -6,7 +6,7 @@ export const getAllCars=()=>async dispatch=>{
     dispatch({type: 'LOADING' , payload:true})
 
     try {
-        const response = await axios.get('http://localhost:5000/cars/getallcars')
+        const response = await axios.get('https://hyper-rental.herokuapp.com/cars/getallcars')
         dispatch({type: 'GET_ALL_CARS', payload:response.data})
         dispatch({type: 'LOADING' , payload:false})
     } catch (error) {
@@ -21,7 +21,7 @@ export const addCar=(reqObj)=>async dispatch=>{
     dispatch({type: 'LOADING' , payload:true})
 
     try {
-         await axios.post('http://localhost:5000/cars/addcar' , reqObj)
+         await axios.post('https://hyper-rental.herokuapp.com/cars/addcar' , reqObj)
        
          dispatch({type: 'LOADING' , payload:false})
          message.success('New car added successfully')
@@ -41,7 +41,7 @@ export const editCar=(reqObj)=>async dispatch=>{
     dispatch({type: 'LOADING' , payload:true})
 
     try {
-         await axios.post('http://localhost:5000/cars/editcar' , reqObj)
+         await axios.post('https://hyper-rental.herokuapp.com/cars/editcar' , reqObj)
        
          dispatch({type: 'LOADING' , payload:false})
          message.success('Car details updated successfully')
@@ -61,7 +61,7 @@ export const deleteCar=(reqObj)=>async dispatch=>{
     dispatch({type: 'LOADING' , payload:true})
 
     try {
-         await axios.post('http://localhost:5000/cars/deletecar' , reqObj)
+         await axios.post('https://hyper-rental.herokuapp.com/cars/deletecar' , reqObj)
        
          dispatch({type: 'LOADING' , payload:false})
          message.success('Car deleted successfully')
